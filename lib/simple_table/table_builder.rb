01cd92918815b262d36609288e3a6e223c018970
niedhui <dianhui.nie@gmail.com>
2012-06-08 17:29:24 +0800

    add some class support

--- lib/simple_table/table_builder.rb
+++ lib/simple_table/table_builder.rb
@@ -1,9 +1,11 @@
 module SimpleTable
   class Column
-    attr_accessor :name
+    attr_accessor :name, :th_class, :td_class
 
     def initialize(name, options = {})
-      @name, @options = name, options
+      @name, @options = name, options.to_options
+      @th_class = @options.delete(:th_class)
+      @td_class = @options.delete(:td_class)
     end
 
     def to_content(model)
@@ -23,18 +25,19 @@ module SimpleTable
       @template, @items = template, items
       @columns , @ops = [], []
       @model_class = options.delete(:model_class)
+      @op_td_class = ([:op] << options.delete(:op_td_class)).join(" ")
       yield self
     end
 
     def render
-      content_tag :table, class: 'table table-bordered' do
+      content_tag :table, class: 'simple-table table table-bordered' do
         render_head + render_body
       end
     end
 
     def render_head
       content_tag :thead do
-        reduce_tags(@columns) { |column| content_tag(:th, @model_class.human_attribute_name(column.name)) } +
+        reduce_tags(@columns) { |column| content_tag(:th, @model_class.human_attribute_name(column.name), class: column.th_class) } +
         content_tag(:th) unless @ops.empty?
       end
     end
@@ -47,15 +50,22 @@ module SimpleTable
 
     def render_tr(item)
       content_tag(:tr) do
-        reduce_tags(@columns) {|column| content_tag(:td, column.to_content(item)) } +
-        reduce_tags(@ops) {|op|  content_tag(:td, template.capture(item,&op), class: :op) }
+        reduce_tags(@columns) {|column| content_tag(:td, column.to_content(item), class: column.td_class) } +
+        reduce_tags(@ops) {|op|  content_tag(:td, template.capture(item,&op), class: @op_td_class) }
       end
     end
 
+    # options:
+    #   th_class
+    #   td_class
     def td(name, options = {})
       @columns << Column.new(name, options)
     end
     alias :column :td
+
+    def op(&block)
+      @ops << block
+    end
 
     def method_missing(name, *args, &block)
       if template.respond_to? name
@@ -65,9 +75,6 @@ module SimpleTable
       end
     end
 
-    def op(&block)
-      @ops << block
-    end
 
     private
 
