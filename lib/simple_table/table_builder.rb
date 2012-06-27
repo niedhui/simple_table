b9d2e4ecbddaaa3accb30b3126b1581b570ab4b4
niedhui <dianhui.nie@gmail.com>
2012-06-27 14:50:15 +0800

    support table class support block for td declare

--- lib/simple_table/table_builder.rb
+++ lib/simple_table/table_builder.rb
@@ -1,43 +1,59 @@
 module SimpleTable
   class Column
     attr_accessor :name, :th_class, :td_class
-
-    def initialize(name, options = {})
-      @name, @options = name, options.to_options
+    
+    # model_class: the model class, not css class
+    # options:
+    #  th_class:  css class
+    #  td_class:  css class
+    def initialize(model_class, name, template, options = {}, &block)
+      @model_class, @name, @template,  @options = model_class, name, template, options.to_options
       @th_class = @options.delete(:th_class)
       @td_class = @options.delete(:td_class)
+      @content_block = block if block_given?
+    end    
+    
+    def label
+      @options[:label] || @model_class.human_attribute_name(name)
     end
-
+    
     def to_content(model)
       content_proc = @options[:content]
       if content_proc
         content_proc.call(model)
+      elsif @content_block
+        @template.capture(model, &@content_block)
       else
         model.send(name)
       end
     end
+    
 
   end
   class TableBuilder
     attr_accessor :template
-
+    
+    # options:
+    #  model_class
+    #  talbe_class
     def initialize(template,items, options = {}, &proc)
       @template, @items = template, items
       @columns , @ops = [], []
       @model_class = options.delete(:model_class)
+      @table_class = (Array(options[:table_class]) << 'simple-table table table-bordered').join(' ')
       @op_td_class = ([:op] << options.delete(:op_td_class)).join(" ")
       yield self
     end
 
     def render
-      content_tag :table, class: 'simple-table table table-bordered' do
+      content_tag :table, class: @table_class  do
         render_head + render_body
       end
     end
 
     def render_head
       content_tag :thead do
-        reduce_tags(@columns) { |column| content_tag(:th, @model_class.human_attribute_name(column.name), class: column.th_class) } +
+        reduce_tags(@columns) { |column| content_tag(:th, column.label, class: column.th_class) } +
         content_tag(:th) unless @ops.empty?
       end
     end
@@ -58,8 +74,9 @@ module SimpleTable
     # options:
     #   th_class
     #   td_class
-    def td(name, options = {})
-      @columns << Column.new(name, options)
+    #   label:
+    def td(name, options = {}, &block)
+      @columns << Column.new(@model_class, name, template, options, &block)
     end
     alias :column :td
 
